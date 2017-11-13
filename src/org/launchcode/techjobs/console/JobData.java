191c4cb4b6bc802ab9ff2daf80c06f1426ba0d68
orphanedcouplet <oliverdayers@gmail.com>
2017-11-13 11:07:16 -0600

    implemented findByValue

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -78,6 +79,29 @@ public class JobData {
 
             if (aValue.contains(value)) {
                 jobs.add(row);
+            }
+        }
+
+        return jobs;
+    }
+
+    public static ArrayList<HashMap<String, String>> findByValue (String value) {
+
+        // load data, if not already loaded
+        loadData();
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> aJob : allJobs) {
+
+            for (Map.Entry<String, String> jobKVPair : aJob.entrySet()) {
+
+                String jobValue = jobKVPair.getValue();
+
+                if (jobValue.contains(value) && !jobs.contains(aJob)) {
+
+                    jobs.add(aJob);
+                }
             }
         }
 
